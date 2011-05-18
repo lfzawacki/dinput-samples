123ec8b6344cc3ef91491486f7aa269701fc0950
Lucas Fialho Zawacki <lfzawacki@gmail.com>
2011-05-17 23:57:49 -0300

    Added huge action mapping example

---
+++ dolphin-dinput8/definitions.h
@@ -0,0 +1,45 @@
+#ifndef _PAD_H_INCLUDED__
+#define _PAD_H_INCLUDED__
+
+#define PAD_ERR_NONE            0
+#define PAD_ERR_NO_CONTROLLER   -1
+#define PAD_ERR_NOT_READY       -2
+#define PAD_ERR_TRANSFER        -3
+
+#define PAD_USE_ORIGIN          0x0080
+#define PAD_BUTTON_LEFT         0x0001
+#define PAD_BUTTON_RIGHT        0x0002
+#define PAD_BUTTON_DOWN         0x0004
+#define PAD_BUTTON_UP           0x0008
+#define PAD_TRIGGER_Z           0x0010
+#define PAD_TRIGGER_R           0x0020
+#define PAD_TRIGGER_L           0x0040
+#define PAD_BUTTON_A            0x0100
+#define PAD_BUTTON_B            0x0200
+#define PAD_BUTTON_X            0x0400
+#define PAD_BUTTON_Y            0x0800
+#define PAD_BUTTON_START        0x1000
+
+typedef void (*TLog)(const char* _pMessage);
+
+typedef struct
+{
+	HWND hWnd;
+	TLog				pLog;
+} SPADInitialize;
+
+typedef struct
+{
+	unsigned short	button;                 // Or-ed PAD_BUTTON_* bits
+	unsigned char	stickX;                 // 0 <= stickX       <= 255
+	unsigned char	stickY;                 // 0 <= stickY       <= 255
+	unsigned char	substickX;              // 0 <= substickX    <= 255
+	unsigned char	substickY;              // 0 <= substickY    <= 255
+	unsigned char	triggerLeft;            // 0 <= triggerLeft  <= 255
+	unsigned char	triggerRight;           // 0 <= triggerRight <= 255
+	unsigned char	analogA;                // 0 <= analogA      <= 255
+	unsigned char	analogB;                // 0 <= analogB      <= 255
+	signed char		err;                    // one of PAD_ERR_* number
+} SPADStatus;
+
+#endif
---
+++ dolphin-dinput8/main.cpp
@@ -0,0 +1,127 @@
+// Original code and license available at:
+// http://code.google.com/p/dolphin-emu/
+
+#include <windows.h>
+#include "definitions.h"
+
+#include "MultiDI.h"
+#include "DIHandler.h"
+
+#include <stdio.h>
+
+CDIHandler g_diHandler;
+SPADStatus g_PadStatus;
+
+void getControllerStatus(SPADStatus* _pPADStatus);
+
+void initializeInput(HWND _hParent)
+{
+	g_diHandler.InitInput(_hParent);
+
+	EnableWindow(_hParent, FALSE);
+	g_diHandler.ConfigInput();
+	EnableWindow(_hParent, TRUE);
+	SetForegroundWindow(_hParent);
+
+}
+
+void getControllerStatus(SPADStatus* _pPADStatus)
+{
+
+	//get keys from dinput
+	g_diHandler.UpdateInput();
+
+    // we just support pad 0 at the time
+	const SControllerInput& ri = g_diHandler.GetControllerInput(0);
+
+    printf("--------------------\n");
+    printf("Buttons S:%d A:%d B:%d X:%d Y:%d Z:%d cLR:%f cUP:%f L:%f R:%f\n",
+    ri.bButtonStart, ri.bButtonA, ri.bButtonB,ri.bButtonX,ri.bButtonY,ri.bButtonZ,
+    ri.fDPadLR, ri.fDPadUP, ri.fTriggerL, ri.fTriggerR );
+
+    printf("<-: %f /\\: %f \\/: %f ->: %f\n", ri.fMainLR,ri.fMainUP,ri.fCPadLR,ri.fCPadUP);
+
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(iMsg) {
+        case WM_CREATE:
+            return 0;
+
+        case WM_PAINT:
+            ValidateRect(hwnd,NULL);
+            return 0;
+
+        case WM_DESTROY:
+            g_diHandler.CleanupDirectInput();
+            PostQuitMessage(0);
+            return 0;
+    }
+
+    return DefWindowProc(hwnd,iMsg,wParam,lParam);
+}
+
+DWORD WINAPI ThreadFunc( LPVOID lpParam )
+{
+    while (true) {
+        // update every half second
+        Sleep(500);
+        getControllerStatus(&g_PadStatus);
+    }
+}
+
+void createCustomWindow(HINSTANCE hInstance, HWND *hwndOut)
+{
+    WNDCLASS wndclass;
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hIcon = LoadIcon(NULL,IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL,IDC_ARROW);
+    wndclass.hInstance = hInstance;
+    wndclass.lpfnWndProc = WindowProc;
+    wndclass.hbrBackground = (HBRUSH) (COLOR_BACKGROUND+1);
+    wndclass.lpszMenuName = NULL;
+
+    wndclass.lpszClassName = "wintest";
+
+    if(!RegisterClass(&wndclass)) {
+        printf("Failed to create window\n");
+        exit(0);
+    }
+
+    *hwndOut = CreateWindow("wintest", "Test", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                   10, 10, 200, 200, NULL, NULL, hInstance, NULL);
+
+}
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hCopy, PSTR szCmd, int iShow)
+{
+    MSG msg;
+    DWORD dwThreadId;
+    HANDLE hThread;
+    HWND hwnd;
+
+    createCustomWindow(hInstance,&hwnd);
+
+    initializeInput(hwnd);
+
+    hThread = CreateThread( NULL, 0, ThreadFunc, NULL, 0, &dwThreadId);
+
+    if (hThread == NULL) {
+      printf("Failed Creating thread\n");
+      exit(0);
+    }
+
+    ShowWindow(hwnd,iShow);
+    UpdateWindow(hwnd);
+
+    while ( GetMessage(&msg, NULL, 0, 0) )
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
